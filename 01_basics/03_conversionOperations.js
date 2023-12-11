ba1e69685112e7c5e6e50f008ea87a50f958753b
venus515 <78139108+venus515@users.noreply.github.com>
2023-12-11 12:00:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score="33"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+//convert string to number
+//"33"=>33
+//"33abs"=>NaN
+// true=>1 false=>0
+
+
+let isLoggedIn=1;
+let boolIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+// covert number to boolean
+// 1=>true;0=>false;
+// ""=>false
+// "venus"=>true
