6591201781fb5d41797a1a69f6dedff3ea45b5c4
venus515 <78139108+venus515@users.noreply.github.com>
2023-12-26 13:18:39 +0000

    string

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,13 @@
+const name="venus"
+const repoCount=50
+
+console.log(name+repoCount+"value"); // this is outdated
+
+console.log(`Hello my name is ${name}. and my repo count is ${repoCount}`); // modern way
+const gameNew= new String('venusgarg')
+console.log(gameNew.length); // o/p: 5
+console.log(gameNew.toUpperCase()); //o/p:VENUS
+const newString =gameNew.substring(0,3)
+console.log(newString);
+const anotherString=gameNew.slice(-9,4)
+console.log(anotherString);
