6bc92721261426ba2fee7fc28c5034f249fcc565
venus515 <78139108+venus515@users.noreply.github.com>
2023-12-26 13:18:06 +0000

    project

---
+++ projects/02/script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+  const msg = document.querySelector('#msg');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+    if(bmi<18.6){
+msg.innerHTML="oops!you are underweight";
+    }
+    else if(bmi>18.6 && bmi<24.9){
+        msg.innerHTML="woww!your weight is normal";
+
+    }
+    else{
+        msg.innerHTML="oops!you are overweight";
+    }
+  }
+
+
+
+});
+
+
